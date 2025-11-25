8da3f3975550c1c14d4f924a95f13e566b7425bf
volkonskaiaem.26 <yekaterina.volkonskaya@gmail.ru>
2025-11-25 10:52:40 +0300

    new GET variable: put

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -2,10 +2,8 @@ package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -24,6 +22,20 @@ public class ApiController {
     public ResponseEntity<String> createBook(@PathVariable("name") String name){
         books.add(name);
         return ResponseEntity.ok("New book:"+name);
+    }
+
+    @DeleteMapping("books/{name}")
+    public ResponseEntity<String> deleteBook(@PathVariable("name") String name){
+        books.remove(name);
+        return ResponseEntity.ok("Deleted book:" + name );
+    }
+
+    @PutMapping("books/{name}/{NewName}")
+    public ResponseEntity<String> putBook(@PathVariable("name") String name,
+                                          @PathVariable("NewName") String NewName){
+        books.remove(name);
+        books.add(NewName);
+        return ResponseEntity.ok("Previous book: "+name+" New book: "+NewName);
     }
 }
 
