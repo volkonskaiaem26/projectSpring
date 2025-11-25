b33935e909e6d5f3c037edee7b5e0ed3d0c2b4cb
volkonskaiaem.26 <yekaterina.volkonskaya@gmail.ru>
2025-11-25 10:32:43 +0300

    new GET variable

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -3,13 +3,26 @@ package com.example.demo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 @RestController
 public class ApiController {
+
+    private List <String> books = new ArrayList<>();
+
     @GetMapping("books")
     public ResponseEntity<String> getText() {
-        return ResponseEntity.ok("Hello text");
+        return ResponseEntity.ok("List of books");
+    }
+
+    @GetMapping("books/{name}")
+    public ResponseEntity<String> createBook(@PathVariable("name") String name){
+        books.add(name);
+        return ResponseEntity.ok("New book:"+name);
     }
 }
 
